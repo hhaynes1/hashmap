09634a5b744021ed58d4d2266d9b4a589c138d37
hhaynes1 <94259379+hhaynes1@users.noreply.github.com>
2024-03-18 16:37:06 -0400

    Implemented all functions for HashMap Class
    Collisions are handled by adding subarray with key/value pair.

--- hashmap.js
+++ hashmap.js
@@ -1,68 +1,148 @@
 export default class HashMap {
-    constructor() {
-
+    constructor(hashArray = new Array(16)) {
+        this.hashArray = hashArray;
+        this.loadFactor = 0.75;
     }
 
-    // take key, produce hash code
+    // take key (string), produce hash code
     hash(key) {
         let hashCode = 0;
-
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.hashArray.length;
         }
-
         return hashCode;
     }
 
     // set or update value assigned to key
     set(key, value) {
-
+        const index = this.hash(key);
+        if (!this.hashArray[index]) {
+            // empty location
+            this.hashArray[index] = [[key, value]];
+        } else {
+            // collision
+            for (let i = 0; i < this.hashArray[index].length; i++) {
+                if (this.hashArray[index][i][0] === key) {
+                    this.hashArray[index][i][1] = value;
+                    return;
+                }
+            }
+            // push new pair into bucket
+            this.hashArray[index].push([key, value]);
+        }
     }
 
     // return value that is assigned to key
     get(key) {
-
+        const index = this.hash(key);
+        for (let i = 0; i < this.hashArray[index].length; i++) {
+            if (this.hashArray[index][i][0] === key) {
+                return this.hashArray[index][i][1];
+            }
+        }
+        return null;
     }
 
     // return true or false if key is in hash map
     has(key) {
-
+        const keys = this.keys();
+        for (let i = 0; i < keys.length; i++) {
+            if (keys[i] === key) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // remove value at key, or return false if not found
     remove(key) {
-
+        const index = this.hash(key);
+        if (this.hashArray[index]) {
+            for (let i = 0; i < this.hashArray[index].length; i++) {
+                if (this.hashArray[index][i][0] === key) {
+                    this.hashArray[index].splice(i, i + 1);
+                    return;
+                }
+            }
+        }
+        return false;
     }
 
     // return number of stored keys
     length() {
-
+        return this.hashArray.length;
     }
 
     // removes all entries
     clear() {
-
+        this.hashArray = new Array(16);
     }
 
     // return array containing all keys
     keys() {
-
+        let keyArray = [];
+        let entries = this.entries();
+        for (let i = 0; i < entries.length; i++) {
+            keyArray.push(entries[i][0]);
+        }
+        return keyArray;
     }
 
     // return array containing all values
     values() {
-
+        let valueArray = [];
+        let entries = this.entries();
+        for (let i = 0; i < entries.length; i++) {
+            valueArray.push(entries[i][1]);
+        }
+        return valueArray;
     }
 
     // return array containing all key/value pairs
+    // [[firstKey, firstValue], [secondKey, secondValue]]
     entries() {
+        let pairArray = [];
+        for (let i = 0; i < this.hashArray.length; i++) {
+            if (this.hashArray[i]) {
+                for (let j = 0; j < this.hashArray[i].length; j++) {
+                    pairArray.push([this.hashArray[i][j][0], this.hashArray[i][j][1]]);
+                }
+            }
+        }
+        return pairArray;
+    }
 
+    // double size of array when size surpasses loadFactor
+    growBuckets() {
+        const newArray = new Array(this.hashArray.length * 2);
+        const entries = this.entries();
+        for (let i = 0; i < entries.length; i++) {
+            const key = entries[i][0];
+            const value = entries[i][1];
+            const index = this.hash(key);
+            if (!newArray[index]) {
+                // empty location
+                newArray[index] = [[key, value]];
+            } else {
+                // push new pair into bucket
+                newArray[index].push([key, value]);
+            }
+        }
+        this.hashArray = newArray;
+    }
+
+    // call growBuckets if hashmap load exceeds loadFactor
+    checkLoad() {
+        let counter = 0;
+        for (let i = 0; i < this.hashArray.length; i++) {
+            if (this.hashArray[i]) {
+                counter++;
+            }
+        }
+        if (counter / this.hashArray.length > this.loadfactor) {
+            console.log('buckets grown');
+            this.growBuckets();
+        }
     }
 }
-
-// provided snippet
-// let index;
-// if (index < 0 || index >= buckets.length) {
-//     throw new Error("Trying to access index out of bound");
-// }
--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 import HashMap from "./hashmap.js";
 
 (() => {
-    
+    let hashmap = new HashMap();
+    console.log(hashmap);
+
+    console.log('\nHASH')
+    console.log(hashmap.hash('abc'));
+    console.log(hashmap.hash('def'));
+    console.log(hashmap.hash('ghi'));
+    console.log(hashmap.hash('jkl'));
+
+    console.log('\nSET');
+    hashmap.set('abc', 1);
+    hashmap.set('def', 2);
+    hashmap.set('ghi', 3);
+    hashmap.set('jkl', 4);
+    hashmap.set('mno', 5);
+    hashmap.set('pqr', 6);
+    hashmap.set('stu', 7);
+    hashmap.set('vwx', 8);
+    hashmap.set('yz', 9);
+    console.log(hashmap);
+
+    console.log('\nGET');
+    console.log(hashmap.get('abc'));
+    console.log(hashmap.get('def'));
+    console.log(hashmap.get('ghi'));
+    console.log(hashmap.get('jkl'));
+
+    console.log('\nHAS');
+    console.log(hashmap.has('abc'));
+    console.log(hashmap.has('def'));
+    console.log(hashmap.has('ghi'));
+    console.log(hashmap.has('jkl'));
+    console.log(hashmap.has('lmn'));
+
+    console.log('\nREMOVE');
+    hashmap.remove('lmn');
+    console.log(hashmap);
+
+    console.log('\nLENGTH');
+    console.log(hashmap.length());
+
+    console.log('\nCLEAR');
+    // hashmap.clear();
+    console.log(hashmap);
+
+    console.log('\nKEYS');
+    console.log(hashmap.keys());
+
+    console.log('\nVALUEs');
+    console.log(hashmap.values());
+
+    console.log('\nENTRIES');
+    console.log(hashmap.entries());
+
+    console.log('\nCHECKLOAD');
+    hashmap.checkLoad();
+
+    console.log('\nGROWBUCKETS')
+    hashmap.growBuckets();
+    console.log(hashmap);
 })();
